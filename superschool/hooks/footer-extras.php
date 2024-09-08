f5e3cdba55934668ee81e83ff53af9a80f994cf7
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-08 16:30:04 +0530

    update

---
+++ superschool/hooks/footer-extras.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>Student Management System &nbsp;<a href="https://www.campcodes.com">Developed By CampCodes</a>.</strong> All rights
+    reserved 2020.
+  </footer>
